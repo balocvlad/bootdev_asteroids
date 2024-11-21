bb0940bcaa23ae2d20cfad836b691cd1a8270ed2
balocvlad <vlad.baloc@yahoo.com>
2024-11-22 00:05:20 +0200

    fix dt scope error

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from constants import *
 from circleshape import *
 from player import *
 
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+
+Player.containers = (updatable, drawable)
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 dt = 0
@@ -13,18 +18,22 @@ def main():
     pygame.init()
     
     
-    
     while True:
+        global dt
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        
+        for update in updatable:
+            update.update(dt)
         screen.fill((250,128,114))
-        player.draw(screen)
+        #player.draw(screen)
+        for draw in drawable:
+            draw.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-        player.update(dt)
+        #player.update(dt)
+       
 
 
     
